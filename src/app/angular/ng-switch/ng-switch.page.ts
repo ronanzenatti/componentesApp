2d4fee6161f5465191ab985a34a46b15f3b98ee3
ronanzenatti <pmboraceia@yahoo.com.br>
2019-05-15 13:28:01 -0300

    ngSwitch

--- src/app/angular/ng-switch/ng-switch.page.ts
+++ src/app/angular/ng-switch/ng-switch.page.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgSwitchPage implements OnInit {
 
+  public tipoUsuario: string = 'usuario';
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { 
   }
 
 }
